838a53d895c58360dbb49c32a69654f1e5fbf1ad
Victor Zhou <vzhou842@gmail.com>
2016-02-05 15:31:55 -0500

    create game -> join room lobby

--- client/js/darkhouse-client.js
+++ client/js/darkhouse-client.js
@@ -4,9 +4,23 @@
  */
 var socket = io.connect();
 
+// ----- User Interaction -----
+
+// Create Game button
 $('#create-game').click(function() {
 	socket.emit('create_game');
 });
+
+// Join Game button
 $('#join-game').click(function() {
 	socket.emit('join_game');
+});
+
+// ----- Communications from Server -----
+
+// Game created by user
+socket.on('game_created', function(data) {
+	$('#main-menu-container').addClass('invisible');
+	$('#room-lobby-container').removeClass('invisible');
+	$('#room-title').text('Room ' + data.gameID);
 });
--- server/darkhouse.js
+++ server/darkhouse.js
@@ -3,12 +3,22 @@ function DarkhouseController(io) {
 }
 
 DarkhouseController.prototype.onConnection = function(socket) {
-	socket.on('create_game', function() {
-		console.log('create game');
-	});
+	socket.on('create_game', createGame);
 	socket.on('join_game', function() {
 		console.log('join game');
 	});
+}
+
+function createGame() {
+	// Create a unique Socket.IO Room
+    var gameID = ( Math.random() * 999999 ) | 0;
+
+    this.emit('game_created', {
+    	gameID: gameID,
+    });
+
+    // Join this socket Room
+    this.join(gameID.toString());
 }
 
 module.exports = DarkhouseController;
