6d85d4deee3be1fab9f455e79abf9774ee8bfe03
Victor Zhou <vzhou842@gmail.com>
2016-02-06 20:51:33 -0500

    indentation: spaces -> tabs

--- client/js/darkhouse-game.js
+++ client/js/darkhouse-game.js
@@ -11,23 +11,23 @@ $('#game-container').append( renderer.domElement );
 var player = new Player();
 scene.add(player.squareMesh);
 
-// set camera depth
+// set camera depth / rotation
 camera.position.z = 25;
 
 document.addEventListener('keydown', function(event) {
-  Key.onKeydown(event);
+	Key.onKeydown(event);
 });
 
 document.addEventListener('keyup', function(event) {
-  Key.onKeyup(event);
+	Key.onKeyup(event);
 });
 
 // render loop
 var render = function () {
-  player.update();
-  player.squareMesh.position.add(player.direction);
-  requestAnimationFrame( render );
-  renderer.render(scene, camera);
+	player.update();
+	player.squareMesh.position.add(player.direction);
+	requestAnimationFrame( render );
+	renderer.render(scene, camera);
 };
 
 render();
--- client/js/darkhouse-player.js
+++ client/js/darkhouse-player.js
@@ -1,52 +1,52 @@
 function Player() {
-  var squareGeometry = new THREE.Geometry(); 
-  squareGeometry.vertices.push(new THREE.Vector3(-1.0,  1.0, 0.0)); 
-  squareGeometry.vertices.push(new THREE.Vector3( 1.0,  1.0, 0.0)); 
-  squareGeometry.vertices.push(new THREE.Vector3( 1.0, -1.0, 0.0)); 
-  squareGeometry.vertices.push(new THREE.Vector3(-1.0, -1.0, 0.0)); 
-  squareGeometry.faces.push(new THREE.Face3(0, 1, 2)); 
-  squareGeometry.faces.push(new THREE.Face3(0, 2, 3)); 
+	var squareGeometry = new THREE.Geometry(); 
+	squareGeometry.vertices.push(new THREE.Vector3(-1.0,  1.0, 0.0)); 
+	squareGeometry.vertices.push(new THREE.Vector3( 1.0,  1.0, 0.0)); 
+	squareGeometry.vertices.push(new THREE.Vector3( 1.0, -1.0, 0.0)); 
+	squareGeometry.vertices.push(new THREE.Vector3(-1.0, -1.0, 0.0)); 
+	squareGeometry.faces.push(new THREE.Face3(0, 1, 2)); 
+	squareGeometry.faces.push(new THREE.Face3(0, 2, 3)); 
 
-  // create white surface material for square
-  var squareMaterial = new THREE.MeshBasicMaterial({
-    color:0xFFFFFF,
-    side:THREE.DoubleSide
-  });
+	// create white surface material for square
+	var squareMaterial = new THREE.MeshBasicMaterial({
+		color:0xFFFFFF,
+		side:THREE.DoubleSide
+	});
 
-  // add material to square + set position
-  this.squareMesh = new THREE.Mesh(squareGeometry, squareMaterial);
-  this.squareMesh.position.set(0.0, 0.0, 0.0);
+	// add material to square + set position
+	this.squareMesh = new THREE.Mesh(squareGeometry, squareMaterial);
+	this.squareMesh.position.set(0.0, 0.0, 0.0);
 
-  // set initial velocities
-  this.direction = new THREE.Vector3(0.0, 0.0, 0.0);
+	// set initial velocities
+	this.direction = new THREE.Vector3(0.0, 0.0, 0.0);
 }
 
 Player.prototype.changeLeftVelocity = function(status) {
-  if (status) this.direction.setX(-0.1);
-  else this.direction.setX(0.0);
+	if (status) this.direction.setX(-0.1);
+	else this.direction.setX(0.0);
 }
 
 Player.prototype.changeRightVelocity = function(status) {
-  if (status) this.direction.setX(0.1);
-  else this.direction.setX(0.0);
+	if (status) this.direction.setX(0.1);
+	else this.direction.setX(0.0);
 }
 
 Player.prototype.changeUpVelocity = function(status) {
-  if (status) this.direction.setY(0.1);
-  else this.direction.setY(0.0);
+	if (status) this.direction.setY(0.1);
+	else this.direction.setY(0.0);
 }
 
 Player.prototype.changeDownVelocity = function(status) {
-  if (status) this.direction.setY(-0.1);
-  else this.direction.setY(0.0);
+	if (status) this.direction.setY(-0.1);
+	else this.direction.setY(0.0);
 }
 
 Player.prototype.update = function() {
-  if (Key.isDown(Key.LEFT)) this.changeLeftVelocity(true);
-  else if (Key.isDown(Key.RIGHT)) this.changeRightVelocity(true)
-  else this.changeLeftVelocity(false);
+	if (Key.isDown(Key.LEFT)) this.changeLeftVelocity(true);
+	else if (Key.isDown(Key.RIGHT)) this.changeRightVelocity(true)
+	else this.changeLeftVelocity(false);
 
-  if (Key.isDown(Key.UP)) this.changeUpVelocity(true);
-  else if (Key.isDown(Key.DOWN)) this.changeDownVelocity(true)
-  else this.changeUpVelocity(false);
+	if (Key.isDown(Key.UP)) this.changeUpVelocity(true);
+	else if (Key.isDown(Key.DOWN)) this.changeDownVelocity(true)
+	else this.changeUpVelocity(false);
 }
