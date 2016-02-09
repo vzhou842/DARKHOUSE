20d38ef4d43bcdf284f8cd7f31e81e90dcda5477
Victor Zhou <vzhou842@gmail.com>
2016-02-09 18:08:36 -0500

    added randomization for obstacle box textures

--- shared/GameObjects/ObstacleBox.js
+++ shared/GameObjects/ObstacleBox.js
@@ -1,16 +1,29 @@
 (function() {
 	var isBrowser = (typeof window !== 'undefined');
 	function _ObstacleBoxFactory(THREE) {
-		// Load an image as texture 
-		var obstacleBoxTexture = (isBrowser) ? new THREE.TextureLoader().load("textures/crate1.png") : null;
+
+		var obstacleBoxTexture1 = (isBrowser) ? new THREE.TextureLoader().load("textures/crate1.png") : null;
+		var obstacleBoxTexture2 = (isBrowser) ? new THREE.TextureLoader().load("textures/crate2.png") : null;
+		var obstacleBoxTexture3 = (isBrowser) ? new THREE.TextureLoader().load("textures/crate3.png") : null;
+		var obstacleBoxTexture4 = (isBrowser) ? new THREE.TextureLoader().load("textures/crate4.png") : null;
+		var obstacleBoxTexture5 = (isBrowser) ? new THREE.TextureLoader().load("textures/crate5.png") : null;
+
+		function randomBoxTexture() {
+			var rand = Math.random();
+			if (rand < 0.2) return obstacleBoxTexture1;
+			else if (rand < 0.4) return obstacleBoxTexture2;
+			else if (rand < 0.6) return obstacleBoxTexture3;
+			else if (rand < 0.8) return obstacleBoxTexture4;
+			else return obstacleBoxTexture5;
+		}
 
 		var boxGeometry = new THREE.BoxGeometry(10, 10, 10);
-		var boxMaterial = new THREE.MeshLambertMaterial({
-			map: obstacleBoxTexture,
-			side: THREE.DoubleSide, // Use this if we want double sided texture rendering
-		});
 
 		function ObstacleBox(originX, originY) {
+			var boxMaterial = new THREE.MeshLambertMaterial({
+				map: randomBoxTexture(),
+				side: THREE.DoubleSide, // Use this if we want double sided texture rendering
+			});
 			THREE.Mesh.call(this, boxGeometry, boxMaterial);
 			this.position.set(originX + 5, originY + 5, 5);
 		}
